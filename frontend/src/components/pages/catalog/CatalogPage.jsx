b7f2e06f9f1aa90f144975be671d16da664ddc7c
Озеров Роман <ozrv@inbox.ru>
2026-06-25 22:50:16 +0500

    added into frontend base for get catalog info

--- frontend/src/components/pages/catalog/CatalogPage.jsx
+++ frontend/src/components/pages/catalog/CatalogPage.jsx
@@ -1,85 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tabs from '@mui/material/Tabs';
 import { Box, Container, Grid } from '@mui/material';
 import ActorCard from './organisms/ActorCard';
 import CategoryTab from '../../shared/tabs/CategoryTab';
+import getPublishedProfile from '../../../services/getProfile/getPublishedProfile';
 
 const CatalogPage = () => {
   const [tabValue, setTabValue] = useState(0);
+  const [actors, setActors] = useState([]);
 
-  const actors = [
-    {
-      id: 1,
-      name: 'Иван Петров',
-      image: 'https://avatars.mds.yandex.net/i?id=65a85fef39b9909ffda1bdac78e2995f_l-3932796-images-thumbs&n=13',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Гитарист',
-      rating: 4.5,
-      reviewCount: 25,
-      price: 2500,
-      description: 'Опытный гитарист с 10-летним стажем. Играет джаз, рок, фолк.',
-      isLiked: false,
-    },
-    {
-      id: 2,
-      name: 'Мария Сидорова',
-      image: 'https://img.magnific.com/free-photo/front-view-handsome-male-musician-holding-saxophone_23-2148730910.jpg?semt=ais_hybrid',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Вокалистка',
-      rating: 4.8,
-      reviewCount: 42,
-      price: 3000,
-      description: 'Профессиональная вокалистка. Специализируюсь на джазе и попсе.',
-      isLiked: false,
-    },
-    {
-      id: 3,
-      name: 'Алексей Волков',
-      image: 'https://avatars.mds.yandex.net/i?id=9bd18b19978921ce65c13d24248a9a4e_l-5231267-images-thumbs&n=13',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Пианист',
-      rating: 4.6,
-      reviewCount: 18,
-      price: 3500,
-      description: 'Классический пианист, готов исполнять композиции любого стиля.',
-      isLiked: false,
-    },
-    {
-      id: 4,
-      name: 'Шикарный басист',
-      image: 'https://i.pinimg.com/736x/78/bc/55/78bc55ecfe53f7abf7e12edbe5b36bc4.jpg',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Гитарист',
-      rating: 5.0,
-      reviewCount: 24,
-      price: 6000,
-      description: 'Просто стоит. Всё равно не слышно.',
-      isLiked: false,
-    },
-    {
-      id: 5,
-      name: 'Ольга Морозова',
-      image: 'https://media.istockphoto.com/id/1470763004/tr/fotoğraf/woman-playing-the-violin.jpg?s=170667a&w=0&k=20&c=lcxBn8eVQzzJ8heecbBmx5cKcs9RCKiXNSGdVXg33YA=',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Скрипачка',
-      rating: 5.0,
-      reviewCount: 33,
-      price: 4000,
-      description: 'Концертирующая скрипачка, сольные выступления и камерная музыка.',
-      isLiked: false,
-    },{
-      id: 6,
-      name: 'Полина Скалл',
-      image: 'https://thumbs.dreamstime.com/b/творчество-и-музыка-молодая-красивая-девушка-играет-на-барабанах-187280556.jpg',
-      avatar: '/avatar-placeholder.jpg',
-      category: 'Барабанщик',
-      rating: 5.0,
-      reviewCount: 40,
-      price: 2500,
-      description: 'Ударные - лучший кайф!',
-      isLiked: false,
+  useEffect(() => {
+    const loadCard = async () => {
+      try {
+        const data = await getPublishedProfile();
+        setActors(data);
+      } catch (err) {
+        console.error('Ошибка загрузки публикаций!', err);
+      }
     }
-  ];
+
+    loadCard();
+  }, []);
 
   const handleContact = (id) => {
     console.log(`Contact requested for actor ${id}`);
